08ea7a40cfcdd481d89c33c313ec3a2e9b3b62a8
eddie <a33707@gmail.com>
2017-09-04 15:23:04 +0900

    작업
    - 작업 페이지 CSS작업

--- js/js.js
+++ js/js.js
@@ -1,5 +1,13 @@
-$(function() {
-	$('.menu').click(function(){
-	  $('.menu_open').show();
-	});
-});
+// (document).ready(function(){ 
+// 	$('.menu-btn').click(function(){
+// 	  if ($('.menu').hasClass('show')) {
+// 			$('.menu').removeClass('show');
+// 			$('.logo').css('color','#000');
+// 			$('.menu-btn').css('color','#000');
+// 		} else {
+// 			$('.menu').addClass('show');
+// 			$('.logo').css('color','#fff');
+// 			$('.menu-btn').css('color','#fff');
+// 		}
+// 	});
+// });
