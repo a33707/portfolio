c19f06b852508f2d1a6163a5e8ab8400fdb6f039
eddie <a33707@gmail.com>
2017-09-14 18:55:19 +0900

    작업
    - 럭시 내용 수정

--- js/js.js
+++ js/js.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){ 
 	$('.menu-btn').click(function(){
 		$('.menu').addClass('show');
-		$('.logo').css('color','#fff');
+		// $('.logo').css('color','#fff');
 		$('.menu-btn').css('display','none');
 		$('.close').css('display','inline-block');
 	});
 	$('.close').click(function(){
 		$('.menu').removeClass('show');
-		$('.logo').css('color','#000');
+		// $('.logo').css('color','#000');
 		$('.menu-btn').css('display','inline-block');
 		$('.close').css('display','none');
 	});
