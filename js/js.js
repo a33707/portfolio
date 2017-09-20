b390cfebe84f902d20431914796924cc687968e2
eddie <a33707@gmail.com>
2017-09-20 15:51:02 +0900

    작업
    - 이미지 클릭 팝업 작업중

--- js/js.js
+++ js/js.js
@@ -31,55 +31,69 @@ $(document).ready(function(){
     });
 
 // 포트폴리오 스크린샷(pfss) 슬라이드로 등장 
-(function($) {
+	(function($) {
 
-  /**
-   * Copyright 2012, Digital Fusion
-   * Licensed under the MIT license.
-   * http://teamdf.com/jquery-plugins/license/
-   *
-   * @author Sam Sehnert
-   * @desc A small plugin that checks whether elements are within
-   *     the user visible viewport of a web browser.
-   *     only accounts for vertical position, not horizontal.
-   */
+	  /**
+	   * Copyright 2012, Digital Fusion
+	   * Licensed under the MIT license.
+	   * http://teamdf.com/jquery-plugins/license/
+	   *
+	   * @author Sam Sehnert
+	   * @desc A small plugin that checks whether elements are within
+	   *     the user visible viewport of a web browser.
+	   *     only accounts for vertical position, not horizontal.
+	   */
 
-  $.fn.visible = function(partial) {
-    
-      var $t            = $(this),
-          $w            = $(window),
-          viewTop       = $w.scrollTop(),
-          viewBottom    = viewTop + $w.height(),
-          _top          = $t.offset().top,
-          _bottom       = _top + $t.height(),
-          compareTop    = partial === true ? _bottom : _top,
-          compareBottom = partial === true ? _top : _bottom;
-    
-    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+	  $.fn.visible = function(partial) {
+	    
+	      var $t            = $(this),
+	          $w            = $(window),
+	          viewTop       = $w.scrollTop(),
+	          viewBottom    = viewTop + $w.height(),
+	          _top          = $t.offset().top,
+	          _bottom       = _top + $t.height(),
+	          compareTop    = partial === true ? _bottom : _top,
+	          compareBottom = partial === true ? _top : _bottom;
+	    
+	    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
-  };
-    
-})(jQuery);
+	  };
+	    
+	})(jQuery);
 
-var win = $(window);
+	var win = $(window);
 
-var allpfss = $('.pfss');
+	var allpfss = $('.pfss');
 
-allpfss.each(function(i, el) {
-  var el = $(el);
-  if (el.visible(true)) {
-    el.addClass('already-visible'); 
-  } 
-});
+	allpfss.each(function(i, el) {
+	  var el = $(el);
+	  if (el.visible(true)) {
+	    el.addClass('already-visible'); 
+	  } 
+	});
 
-win.scroll(function(event) {
-  
-  allpfss.each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      el.addClass('come-in'); 
-    } 
-  });
-  
-});
+	win.scroll(function(event) {
+	  allpfss.each(function(i, el) {
+	    var el = $(el);
+	    if (el.visible(true)) {
+	      el.addClass('come-in'); 
+	    } 
+	  });
+	});
+
+	// 이미지팝업
+	$('.image-popup').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
 });
