74fee1bcadf1c968c5bd49f6ae28d9af531a2159
a33707 <30279362+a33707@users.noreply.github.com>
2020-09-22 16:29:34 +0900

    Add files via upload

---
+++ bgnsluck/js/bgnsluck.js
@@ -0,0 +1,27 @@
+$(document).ready( function() {
+
+  $(window).scroll(function() {
+    var value = $(this).scrollTop();  //스크롤 수치를 취득
+    console.log(value);
+  });
+
+skrollr.init({
+	easing: 'cubic'
+});
+
+
+
+
+// 반응형
+var desMobile = function (){
+	var width_size = $(window).width();
+
+	if (width_size <= 640) {
+
+	} else {
+
+	};
+
+}
+
+});
